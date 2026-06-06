e70b39cb15919a156ce9ba5de67ad919fc8ea4bb
MuhammadAlfathi <muhammad.alfathi@binus.ac.id>
2026-06-06 18:06:03 +0700

    Add files via upload

---
+++ config/holidays.php
@@ -0,0 +1,99 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Indonesia Public Holidays
+    |--------------------------------------------------------------------------
+    | Format: Y-m-d
+    | Includes:
+    | - National holidays (libur nasional)
+    | - Collective leave (cuti bersama) for ASN (2025–2026 only)
+    */
+
+    2025 => [
+        // National Holidays
+        '2025-01-01', // New Year
+        '2025-01-27', // Isra Mi'raj
+        '2025-01-29', // Chinese New Year
+        '2025-03-29', // Nyepi
+        '2025-03-31', // Idul Fitri
+        '2025-04-01', // Idul Fitri
+        '2025-04-18', // Good Friday
+        '2025-04-20', // Easter
+        '2025-05-01', // Labour Day
+        '2025-05-12', // Vesak
+        '2025-05-29', // Ascension Day
+        '2025-06-01', // Pancasila Day
+        '2025-06-06', // Eid al-Adha
+        '2025-06-27', // Islamic New Year
+        '2025-08-17', // Independence Day
+        '2025-09-05', // Maulid Nabi
+        '2025-12-25', // Christmas
+
+        // Cuti Bersama (ASN)
+        '2025-01-28', // Imlek
+        '2025-03-28', // Nyepi
+        '2025-04-02', // Idul Fitri
+        '2025-04-03', // Idul Fitri
+        '2025-04-04', // Idul Fitri
+        '2025-04-07', // Idul Fitri
+        '2025-05-13', // Vesak
+        '2025-05-30', // Ascension Day
+        '2025-06-09', // Eid al-Adha
+        '2025-12-26', // Christmas
+    ],
+
+    2026 => [
+        // National Holidays
+        '2026-01-01', // New Year
+        '2026-01-16', // Isra Mi'raj
+        '2026-02-17', // Chinese New Year
+        '2026-03-19', // Nyepi
+        '2026-03-21', // Idul Fitri
+        '2026-03-22', // Idul Fitri
+        '2026-04-03', // Good Friday
+        '2026-04-05', // Easter
+        '2026-05-01', // Labour Day
+        '2026-05-14', // Ascension Day
+        '2026-05-27', // Eid al-Adha
+        '2026-05-31', // Vesak
+        '2026-06-01', // Pancasila Day
+        '2026-06-16', // Islamic New Year
+        '2026-08-17', // Independence Day
+        '2026-08-25', // Maulid Nabi
+        '2026-12-25', // Christmas
+
+        // Cuti Bersama (ASN)
+        '2026-02-16', // Imlek
+        '2026-03-18', // Nyepi
+        '2026-03-20', // Idul Fitri
+        '2026-03-23', // Idul Fitri
+        '2026-03-24', // Idul Fitri
+        '2026-05-15', // Ascension Day
+        '2026-05-28', // Eid al-Adha
+        '2026-12-24', // Christmas
+    ],
+
+    2027 => [
+        // National Holidays ONLY (no cuti bersama)
+        // ⚠️ Islamic & lunar dates are estimated
+        '2027-01-01', // New Year
+        '2027-01-26', // Isra Mi'raj (estimated)
+        '2027-02-06', // Chinese New Year
+        '2027-03-08', // Nyepi
+        '2027-03-10', // Idul Fitri
+        '2027-03-11', // Idul Fitri
+        '2027-04-02', // Good Friday
+        '2027-05-01', // Labour Day
+        '2027-05-19', // Vesak (estimated)
+        '2027-05-27', // Ascension Day (estimated)
+        '2027-06-01', // Pancasila Day
+        '2027-06-26', // Islamic New Year (estimated)
+        '2027-08-17', // Independence Day
+        '2027-09-14', // Maulid Nabi (estimated)
+        '2027-12-25', // Christmas
+    ],
+
+];
